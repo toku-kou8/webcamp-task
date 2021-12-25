d1d76bdb2a7482f13e5c9b5e5ae787df3eda5c9f
toku-kou8 <koichi.university@outlook.jp>
2021-12-25 12:24:01 +0900

    finish 1~3

---
+++ javascript/capture2/app.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 50;
+let c = 5;
+let d = 100;
+
+alert(a+'+'+b+'='+(a+b));
+alert(a+'-'+c+'='+(a-c));
+alert(a+'x'+c+'='+(a*c));
+alert(d+'÷'+b+'='+(d/b));
---
+++ javascript/capture3/sample.js
@@ -0,0 +1,18 @@
+var nickname = "taro"
+console.log(nickname)
+
+var nickname = "ichiro"
+console.log(nickname)
+
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var y = "hello"
+}
+
+foo();
+
+for (let i = 0; i < 10; i++){
+  console.log(i);
+}
