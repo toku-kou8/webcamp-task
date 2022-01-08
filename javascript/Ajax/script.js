b509d2f63fc3ad99bdcd3099aedd5d0a62020757
toku-kou8 <koichi.university@outlook.jp>
2022-01-08 22:20:16 +0900

    ajax

---
+++ javascript/Ajax/script.js
@@ -0,0 +1,28 @@
+const API_KEY = 'f500af2286e2703381ad4ba81d9e3864'
+
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'json',
+    }).done(function (data){
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+      alert('通信に失敗しました。');
+    });
+  });
+});
